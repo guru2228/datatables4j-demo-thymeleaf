e84334f4da1828e3056026defd79ff623f99ff8e
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-01-03 18:53:39 +0100

    New examples
    Demo based on the first one : demo-jsp
    Default and Bootstrap2 examples added
    Templates created

--- src/main/java/com/github/datatables4j/demo/thymeleaf/controller/DemoController.java
+++ src/main/java/com/github/datatables4j/demo/thymeleaf/controller/DemoController.java
@@ -5,17 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.datatables4j.demo.thymeleaf.entity.Person;
 import com.github.datatables4j.demo.thymeleaf.service.PersonService;
 
 /**
- * Controller for the basic examples management.
+ * Controllers for the all demo examples.
  * 
- * @author Thibault Duchateau
+ * @author tduchateau
  */
 @Controller
+@RequestMapping(method = RequestMethod.GET)
 public class DemoController {
 	
 	@Autowired
@@ -31,4 +35,33 @@ public class DemoController {
         return "index";
     }
 	
+	@RequestMapping(value="/personsWs", method = RequestMethod.GET)
+	public @ResponseBody List<Person> getPersonss(){
+		return personCoordinator.get100();
+	}
+	
+	@RequestMapping(value="/basic/{page}")
+	public String goToBasicExample(@PathVariable String page){
+		return "basic/" + page;
+	}
+	
+	@RequestMapping(value="/advanced/{page}")
+	public String goToAdvancedExample(@PathVariable String page){
+		return "advanced/" + page;
+	}
+	
+	@RequestMapping(value="/features/{page}")
+	public String goToFeatureExample(@PathVariable String page){
+		return "features/" + page;
+	}
+	
+	@RequestMapping(value="/themes/{page}")
+	public String goToThemeExample(@PathVariable String page){
+		return "themes/" + page;
+	}
+	
+	@RequestMapping(value="/plugins/{page}")
+	public String goToPluginExample(@PathVariable String page){
+		return "plugins/" + page;
+	}
 }
